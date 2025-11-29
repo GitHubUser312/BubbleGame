994389f8bc25cbe12830d61ff507f241da9fb638
Gary Lai <awesomegaryomg@gmail.com>
2025-11-29 12:30:12 -0800

    Testing key presses + loads of debug
    needs to figure out how to log any key press other than none
    lots of delta time debug useful for figuring out timers

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    enum State
+    {
+        Frozen,
+        Chasing,
+    }
+
     [SerializeField]
     private GameObject popEffect;
     [SerializeField]
@@ -11,28 +17,63 @@ public class EnemyController : MonoBehaviour
     private float speed = 4;
 
     private int health = 100;
-    private Rigidbody2D rigid;
+    float freezeTime = 3.0f;
+    float waitTimer = 0.0f;
+    private Rigidbody2D rb;
+    State state = State.Chasing;
     void Awake()
     {
         if (player == null)
         {
             player = GameObject.FindGameObjectWithTag("Player");
         }
-        rigid = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        // facing the player
-        Vector2 direction = (transform.position - player.transform.position).normalized;
-        //rigid.linearVelocity = -direction * speed;
-        rigid.MovePosition(rigid.position - direction * speed * Time.fixedDeltaTime);
+        ChasePlayer();
+        //switch (state)
+        //{
+        //    case State.Frozen:
+        //        Frozen();
+        //        break;
+        //    case State.Chasing:
+        //        ChasePlayer();
+        //        break;
+        //    default:
+        //        Debug.LogError("Unknown state!");
+        //        break;
+        //}
     }
     // Update is called once per frame
     private void OnTriggerExit2D(Collider2D collision)
     {
         Destroy(gameObject);
         Instantiate(popEffect, transform.position, Quaternion.identity);
+    }
+
+    private void Frozen()
+    {
+        // freeze for some time
+
+
+        //waitTimer -= Time.fixedDeltaTime; //?Time.deltaTime;
+        //if (waitTimer > Time.deltaTime + freezeTime)
+        //{
+        //    state = State.Chasing;
+        //}
+
+
+
+
+    }
+    private void ChasePlayer()
+    {
+        // facing the player
+        Vector2 direction = (transform.position - player.transform.position).normalized;
+        //rigid.linearVelocity = -direction * speed; // does the same thing
+        rb.MovePosition(rb.position - speed * Time.fixedDeltaTime * direction);
     }
 
     IEnumerator TakeDamage(int damage, int delay)
@@ -45,5 +86,16 @@ public class EnemyController : MonoBehaviour
     {
         Destroy(gameObject);
         Instantiate(popEffect, transform.position, Quaternion.identity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 v2;
+        v2.y = 10;
+        v2.x = 10;
+
+        Debug.Log("Player OnCollisionEnter2D with " + collision.gameObject.name);
+        // bounce away from player
+        rb.linearVelocity = (-rb.linearVelocity * 20) - v2;
     }
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 using UnityEngine.Windows;
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +12,11 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction = null;
 
     private Rigidbody2D rb = null;
+
+    // Delta time tracking
+    private float localDelta = 0.0f;
+    // wait timer for delta time tracking
+    private int deltaWait = 1;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -38,7 +45,14 @@ public class PlayerController : MonoBehaviour
         // moveInput = left jobstick / d-pad
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
 
-        Debug.Log("Player moveInput : " + moveInput);
+        localDelta += Time.deltaTime;
+        //Debug.Log("Delta : " + localDelta + ", " + (int)localDelta);
+
+        //if ((int)localDelta % deltaWait == 0)
+        //{ Debug.Log("DeltaTime : " + Time.deltaTime); }
+
+        //if ((int)localDelta % deltaWait == 0)
+        //{ Debug.Log("Player moveInput : " + moveInput); }
 
         //Vector2 fwd = rb.transform.forward;
 
@@ -46,11 +60,33 @@ public class PlayerController : MonoBehaviour
 
         Vector2 moveVelocity = moveInput * fspeed;
 
-        Debug.Log("Player moveVelocity : " + moveVelocity);
+        //if ((int)localDelta % deltaWait == 0)
+        //{ Debug.Log("Player moveVelocity : " + moveVelocity); }
 
         rb.linearVelocity = moveVelocity;
         //rb.angularVelocity = 0.0f;
 
-
+        OnKeyDownEvent(new KeyDownEvent());
     }
+
+    void OnKeyDownEvent(KeyDownEvent e)
+    {
+        if (e.keyCode != KeyCode.None)
+        {
+            Debug.Log("Key Down: " + e.keyCode);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 v2;
+        v2.y = 10;
+        v2.x = 10;
+
+        Debug.Log("Player OnCollisionEnter2D with " + collision.gameObject.name);
+        // bounce away from enemy
+        rb.linearVelocity = (-rb.linearVelocity * 20) - v2;
+    }
+
 }
