d5a633e1ef57fd2a98394db16c8fc4876c9d1a9f
Aung Phone Pyae <139940691+GitHubUser312@users.noreply.github.com>
2025-12-18 23:03:46 -0800

    new changes

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
